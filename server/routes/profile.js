f7c0a96c13ce96abfb20b10fc5911bcb9de7f473
Nathan Brickett <natey37@gmail.com>
2022-01-29 13:12:49 -0800

    install multer for image uploads

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -4,10 +4,13 @@ const protect = require('../middleware/auth');
 const {
   editProfile,
   loadProfile,
+  uploadProfilePhoto
 } = require('../controllers/profile');
 
 router.route('/edit').put(protect, editProfile);
 
 router.route('/load').get(protect, loadProfile);
+
+router.route('/upload').get(uploadProfilePhoto)
 
 module.exports = router;
