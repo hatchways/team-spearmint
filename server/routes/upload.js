2308d7eda91f187a861a246b681b829cbd4b583d
Nathan Brickett <natey37@gmail.com>
2022-01-30 20:44:07 -0800

    add uploadImage route that allows a user to upload a photo to an S3 bucket and save this url location on their profile photo

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,84 +1,47 @@
-// const Profile = require("../models/Profile");
-// const express = require("express");
-// const asyncHandler = require("express-async-handler");
-// const upload = require("../utils/uploadFile");
-// const multer  = require('multer')
-// const router = express.Router();
-// const path = require('path');
-
-// // @route POST /profile/upload
-// // @desc Post user profile photo
-// // @access Private
-
-// const imageStorage = multer.diskStorage({
-//     // Destination to store image     
-//     destination: '../images', 
-//     filename: (req, file, cb) => {
-//       cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname))
-//       // file.fieldname is name of the field (image)
-//       // path.extname get the uploaded file extension
-//     }
-//   });
-  
-//   const imageUpload = multer({
-//     storage: imageStorage,
-//     limits: {
-//       fileSize: 10000000 // 1000000 Bytes = 1 MB
-//     },
-//     fileFilter(req, file, cb) {
-//       if (!file.originalname.match(/\.(png|jpg)$/)) { 
-//          // upload only png and jpg format
-//          return cb(new Error('Please upload a Image'))
-//        }
-//      cb(undefined, true)
-//   }
-//   }) 
-  
-//     router.post('/uploadPhoto', imageUpload.single('name'), (req, res) => {
-//         console.log(req.file);
-//         res.status(200).send(req.file)
-//     }, (error, req, res, next) => {
-//         res.status(400).send({ error: error.message })
-//     })
-
-// module.exports = router;
-
+const Profile = require("../models/Profile");
 const express = require('express');
+const AWS = require('aws-sdk');
 const multer = require('multer');
-const path = require('path');
+const   multerS3 = require('multer-s3');
 const router = express.Router()
-const uploadFile = require("../utils/uploadFile");
+require('dotenv').config();
 
-// Image Upload
-const imageStorage = multer.diskStorage({
-    destination: 'images', // Destination to store image 
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname))
-        // file.fieldname is name of the field (image), path.extname get the uploaded file extension
-    }
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ID,
+  secretAccessKey: process.env.AWS_KEY
 });
 
-const imageUpload = multer({
-    storage: imageStorage,
-    limits: {
-        fileSize: 1000000   // 1000000 Bytes = 1 MB
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(png|jpg)$/)) {     // upload only png and jpg format
-            return cb(new Error('Please upload a Image'))
-        }
-        cb(undefined, true)
+var upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.BUCKET_NAME,
+    key: function (req, file, cb) {
+      cb(null, file.originalname); 
     }
-})  
+  })
+});
 
-// For Single image upload
-router.post('/uploadImage', imageUpload.single('image'), (req, res) => {
-    // uploadFile(req.file)
-    res.send(req.file)
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
+router.post('/uploadImage', upload.single('image'), function(req, res, next) {
+  if (!req.file) {
+    res.status(400).send("Trouble uploading image, please try again!")
+  } else {
+    Profile.findOne({ userId: req.body.id }, (error, currentProfile) => {
+      if (!currentProfile) {
+        res.status(400).send("Profile not found!")
+      } else {
+        currentProfile.photo = req.file.location
+
+        currentProfile.save(function (error) {
+          if (error) {
+            res.status(400).send(error)
+          } else {
+            res.status(200).send("Image successfully uploaded and saved to profile!")
+          }
+        });
+      }
+    })
+  }
+});
 
 
 module.exports = router;
-
