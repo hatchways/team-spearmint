3bd82a1fb6f04eb822f9d40fa45fbee2afffce09
Nathan Brickett <natey37@gmail.com>
2022-02-03 18:59:47 -0800

    add delete image route

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const   multerS3 = require('multer-s3');
 const router = express.Router()
 require('dotenv').config();
+const deletePhoto = require("../utils/deletePhoto")
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ID,
@@ -21,11 +22,13 @@ var upload = multer({
   })
 });
 
-router.post('/uploadImage', upload.single('image'), function(req, res, next) {
+router.post('/uploadImage/:id', upload.single('image'), function(req, res, next) {
+  console.log(req.file)
   if (!req.file) {
+    console.log('in here');
     res.status(400).send("Trouble uploading image, please try again!")
   } else {
-    Profile.findOne({ userId: req.body.id }, (error, currentProfile) => {
+    Profile.findOne({ userId: req.params.id }, (error, currentProfile) => {
       if (!currentProfile) {
         res.status(400).send("Profile not found!")
       } else {
@@ -42,6 +45,30 @@ router.post('/uploadImage', upload.single('image'), function(req, res, next) {
     })
   }
 });
+
+router.post('/deleteImage/:key', async function(req, res, next) {
+  const profile = await Profile.findById(req.body.id);
+
+  if(profile){
+    profile.photo = null 
+    profile.save(function (error) {
+      if (error) {
+        res.status(400).send(error)
+      } else {
+        deletePhoto(req.params.key)
+        .then((resp) => {
+          console.log("successfully deleted")
+          res.status(200).send('deleted')
+        })
+        .catch((error) => {
+          res.status(400).send(error)
+        })
+      }
+    });
+  } else {
+    res.status(400).send("Profile not found")
+  }
+})
 
 
 module.exports = router;
