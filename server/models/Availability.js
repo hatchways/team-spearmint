16867ade3a00865515363a92f12d4e963e12f113
Nathan Brickett <natey37@gmail.com>
2022-02-07 13:27:25 -0800

    create availability schema

---
+++ server/models/Availability.js
@@ -0,0 +1,97 @@
+const mongoose = require("mongoose");
+
+const availabilitySchema = new mongoose.Schema({
+  profileId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Profile'
+  },
+  schedule: {
+    'monday' : {
+      startTime: {
+        type: Date
+      }, 
+      endTime: {
+        type: Date
+      },
+      active: {
+        type: Boolean,
+        default: false 
+      }
+    }, 
+    'tuesday' : {
+      startTime: {
+        type: Date
+      }, 
+      endTime: {
+        type: Date
+      },
+      active: {
+        type: Boolean,
+        default: false 
+      }
+    }, 
+    'wednesday' : {
+      startTime: {
+        type: Date
+      }, 
+      endTime: {
+        type: Date
+      },
+      active: {
+        type: Boolean,
+        default: false 
+      }
+    }, 
+    'thursday' : {
+      startTime: {
+        type: Date
+      }, 
+      endTime: {
+        type: Date
+      },
+      active: {
+        type: Boolean,
+        default: false 
+      }
+    }, 
+    'friday' : {
+      startTime: {
+        type: Date
+      }, 
+      endTime: {
+        type: Date
+      },
+      active: {
+        type: Boolean,
+        default: false 
+      }
+    }, 
+    'saturday' : {
+      startTime: {
+        type: Date
+      }, 
+      endTime: {
+        type: Date
+      },
+      active: {
+        type: Boolean,
+        default: false 
+      }
+    }, 
+    'sunday' : {
+      startTime: {
+        type: Date
+      }, 
+      endTime: {
+        type: Date
+      },
+      active: {
+        type: Boolean,
+        default: false 
+      }
+    }
+  },
+});
+
+module.exports = Availability = mongoose.model("availability", availabilitySchema);
