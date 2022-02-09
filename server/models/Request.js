f5b8487d5e47ca40a92484254e2221755b9d8829
Sebastien Laifa <seblaifa@hotmail.com>
2022-02-08 17:50:42 -0800

    removed some request model properties and added date validator

--- server/models/Request.js
+++ server/models/Request.js
@@ -5,66 +5,53 @@ const requestSchema = new mongoose.Schema(
         ownerId: {
             type: mongoose.Schema.Types.ObjectId,
             required: true,
-            ref: 'User'
-        },
-        ownerName: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        ownerPhoto: {
-            type: String,
-            required: true,
+            ref: "User",
         },
         sitterId: {
             type: mongoose.Schema.Types.ObjectId,
             required: true,
-            ref: 'User'
-        },
-        sitterName: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        location: {
-            type: String,
-            default: '',
+            ref: "User",
         },
         start: {
             type: Date,
             required: true,
             default: Date.now,
+            min: [new Date(), "Staring date should be greater or equal than today"],
         },
         end: {
             type: Date,
             required: true,
-            default: () => Date.now() + 24*60*60*1000,
+            default: () => Date.now() + 60 * 60 * 1000,
+            validate: [dateValidator, "Start Date must be less than End Date"],
         },
         animalType: {
             type: String,
-            enum: ['dog', 'cat', 'other'],
+            enum: ["dog", "cat", "other"],
             required: true,
         },
         usefulInfo: {
             type: String,
-            default: '',
+            default: "",
         },
-        accepted: {
-            type: Boolean,
-            default: false,
-        },
-        declined: {
-            type: Boolean,
-            default: false,
+        status: {
+            type: String,
+            enum: ["pending", "accepted", "declined", "completed"],
+            default: "pending",
+            lowercase: true,
+            required: true,
         },
         paid: {
             type: Boolean,
             default: false,
-        }
+        },
     },
     {
-        timestamps: true
+        timestamps: true,
     }
 );
+
+function dateValidator(value) {
+    return this.startDate <= value;
+}
 
 module.exports = Request = mongoose.model("Request", requestSchema);
