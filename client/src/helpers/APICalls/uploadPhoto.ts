6cd6180e46f4c8043449345b338d10660f315ab2
Nathan Brickett <natey37@gmail.com>
2022-02-10 11:57:39 -0800

    fix issues

--- client/src/helpers/APICalls/uploadPhoto.ts
+++ client/src/helpers/APICalls/uploadPhoto.ts
@@ -5,11 +5,10 @@ import { User } from '../../interface/User';
 const uploadPhoto = async (data: FormData, user: User | undefined): Promise<AuthApiData> => {
   const fetchOptions: FetchOptions = {
     method: 'POST',
-    // headers: { 'Content-Type': 'application/json' },
     body: data,
     credentials: 'include',
   };
-  return await fetch(`/uploadImage/${user?.id}`, fetchOptions)
+  return await fetch(`/upload-image/${user?.id}`, fetchOptions)
     .then((res) => res.json())
     .catch(() => ({
       error: { message: 'Unable to connect to server. Please try again' },
