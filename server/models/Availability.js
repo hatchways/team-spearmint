56989d921c9fdc664d3e11c14c5625cee08e85e4
Nathan Brickett <natey37@gmail.com>
2022-02-14 13:07:09 -0800

    fix most issues

--- server/models/Availability.js
+++ server/models/Availability.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 
-const dateOptions = {
+const dayOptions = {
     startTime: {
-      type: Date, 
+      type: Number, 
       required: [
         { validator: isDayActive, msg: 'A start time is required if this day is active!'},
         { validator: isValidTime, msg: 'The start time cannot be after the end time'}
-      ]
+      ], 
+      default: 360 //6AM
     }, 
     endTime: {
-      type: Date,
+      type: Number,
       required: [
         { validator: isDayActive, msg: 'A end time is required if this day is active!'},
         { validator: isValidTime, msg: 'The end time cannot be before the start time'}
-      ]
+      ], 
+      default: 1260 //9PM
     },
     active: {
       type: Boolean,
@@ -26,7 +28,7 @@ function isDayActive(){
 }
 
 function isValidTime(){
-  return this.startTime > this.endTime
+  return this.startTime < this.endTime
 }
 
 const availabilitySchema = new mongoose.Schema({
@@ -35,18 +37,13 @@ const availabilitySchema = new mongoose.Schema({
     required: true,
     ref: 'Profile'
   },
-  monday: dateOptions,
-  tuesday: dateOptions,
-  wednesday: dateOptions,
-  thursday: dateOptions,
-  friday: dateOptions,
-  saturday: dateOptions,
-  sunday: dateOptions,
+  monday: dayOptions,
+  tuesday: dayOptions,
+  wednesday: dayOptions,
+  thursday: dayOptions,
+  friday: dayOptions,
+  saturday: dayOptions,
+  sunday: dayOptions,
 });
-
-availabilitySchema.pre('save', function(next) {
-  let newTime = this.startTime
-  
-})
 
 module.exports = Availability = mongoose.model("Availability", availabilitySchema);
