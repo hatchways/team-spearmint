f0c169d05425f44fd38e11256f296e925cf1832a
Sebastien Laifa <seblaifa@hotmail.com>
2022-02-15 07:39:46 -0800

    notification model

---
+++ server/models/Notification.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+    {
+        type: {
+            type: String,
+            enum: ["system", "user", "event", "message"],
+            default: "system",
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        message: {
+            type: String,
+            required: true,
+        },
+        isRead: {
+            type: Boolean,
+            default: false,
+        },
+        readAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        senderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
+        recipientId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = Notification = mongoose.model("Notification", notificationSchema);
