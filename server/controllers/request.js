3ef6a9689ba4cb3241d77bdff407f725d2fcbecc
Sebastien Laifa <77294017+seblaifa3315@users.noreply.github.com>
2022-02-16 10:29:07 -0800

    Update server/controllers/request.js
    Co-authored-by: Rajiv Titus <75184965+rajivtitus@users.noreply.github.com>

--- server/controllers/request.js
+++ server/controllers/request.js
@@ -13,7 +13,7 @@ exports.createRequest = async (req, res) => {
 
         const request = await Request.create(req.body);
 
-        res.status(200).json(request);
+        res.status(201).json(request);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
