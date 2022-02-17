c4501229c8b254593accf8e8cfab54ff41371b7d
Sebastien Laifa <seblaifa@hotmail.com>
2022-02-17 10:20:18 -0800

    not relevant

--- server/models/Request.js
+++ server/models/Request.js
@@ -16,7 +16,7 @@ const requestSchema = new mongoose.Schema(
             type: Date,
             required: true,
             default: Date.now,
-            min: [new Date(), "Staring date should be greater or equal than today"],
+            min: [new Date(), "Starting date should be greater or equal than today"],
         },
         end: {
             type: Date,
