5829901862f7bad0842e335815788bde8f3ed490
Nathan Brickett <natey37@gmail.com>
2022-02-18 17:26:52 -0800

    add semi functionality for scheduling, added creation, make active schedule, still need to add delete, and edit?

--- client/src/pages/Settings/Availability/Availability.tsx
+++ client/src/pages/Settings/Availability/Availability.tsx
@@ -15,6 +15,9 @@ import { Formik } from 'formik';
 import FormInput from '../../../components/FormInput/FormInput';
 import { useSnackBar } from '../../../context/useSnackbarContext';
 import StarIcon from '@mui/icons-material/Star';
+import createSchedule from '../../../helpers/APICalls/createSchedule';
+import getAllSchedules from '../../../helpers/APICalls/getAllSchedules';
+import makeActiveSchedule from '../../../helpers/APICalls/makeActiveSchedule';
 
 interface Props {
   header: string;
@@ -26,60 +29,159 @@ interface DayOfWeekInputProps {
   setFieldValue: (field: string, value: any, shouldValidate?: boolean | undefined) => void;
 }
 
+interface Day {
+  active: boolean;
+  startTime: number;
+  endTime: number;
+}
+
+interface Schedule {
+  [day: string]: Day | string;
+}
+
+interface schedule {
+  name: string;
+  monday: Day;
+  tuesday: Day;
+  wednesday: Day;
+  thursday: Day;
+  friday: Day;
+  saturday: Day;
+  sunday: Day;
+}
+
+const daysOfTheWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+const times = [
+  '00:00',
+  '01:00',
+  '02:00',
+  '03:00',
+  '04:00',
+  '05:00',
+  '06:00',
+  '07:00',
+  '08:00',
+  '09:00',
+  '10:00',
+  '11:00',
+  '12:00',
+  '13:00',
+  '14:00',
+  '15:00',
+  '16:00',
+  '17:00',
+  '18:00',
+  '19:00',
+  '20:00',
+  '21:00',
+  '22:00',
+  '23:00',
+  '24:00',
+];
+
 export default function Availability({ header }: Props): JSX.Element {
   const { updateSnackBarMessage } = useSnackBar();
   const classes = useStyles();
-  const daysOfTheWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-  const [newSchedule, setNewSchedule] = useState(false);
+  const [schedules, setSchedules] = useState<any[]>([]);
+  const [schedule, setSchedule] = useState<schedule>();
+  const [showActive, setShowActive] = useState(false);
 
+  console.log(schedules);
+
+  useEffect(() => {
+    const sanitizeSchedules = (schedules: any) => {
+      schedules.forEach((schedule: any) => {
+        daysOfTheWeek.forEach((day) => {
+          const convertedStartTime = times[schedule[day].startTime / 60];
+          const convertedEndTime = times[schedule[day].endTime / 60];
+          schedule[day].startTime = convertedStartTime;
+          schedule[day].endTime = convertedEndTime;
+        });
+      });
+      return schedules;
+    };
+
+    const getSchedules = async () => {
+      const allSchedules = await getAllSchedules();
+
+      if (!allSchedules) {
+        updateSnackBarMessage('Error loading schedules!');
+      } else {
+        setSchedules(sanitizeSchedules(allSchedules));
+      }
+    };
+    getSchedules();
+  }, [updateSnackBarMessage]);
+
+  const handleMakeActiveSchedule = (scheduleId: any) => {
+    // debugger;
+    const id = typeof scheduleId === 'object' ? scheduleId._id : scheduleId;
+    makeActiveSchedule(id).then((res) => {
+      console.log(res);
+      setShowActive(true);
+    });
+    updateSnackBarMessage('Schedule is now active!');
+  };
+
+  const handleEditSchedule = (e: any) => {
+    const sanitizedSchedule = sanitizeTimes(e);
+  };
+
+  console.log(schedules);
+  const [newSchedule, setNewSchedule] = useState(false);
+  const [newScheduleCreated, setNewScheduleCreated] = useState(false);
+  const [newScheduleId, setNewScheduleId] = useState<string>('');
   const handleNewSchedule = () => {
     setNewSchedule(true);
   };
 
-  const handleSubmit = () => {
-    //
+  const handleNewSubmit = (e: any) => {
+    console.log(e);
+    const sanitizedSchedule = sanitizeTimes(e);
+    createSchedule(sanitizedSchedule).then((res) => {
+      console.log(res);
+      setNewScheduleId(res._id);
+    });
+    updateSnackBarMessage('Your new schedule was created!');
+    setNewScheduleCreated(true);
+  };
+
+  const sanitizeTimes = (schedule: any) => {
+    const sanitizedSchedule = {} as Schedule;
+    sanitizedSchedule['name'] = schedule.name;
+    daysOfTheWeek.forEach((day) => {
+      if (schedule[day].active) {
+        const startTimeInMinutes = parseInt(schedule[day].startTime) * 60;
+        const endTimeInMinutes = parseInt(schedule[day].endTime) * 60;
+
+        sanitizedSchedule[day] = {
+          active: true,
+          startTime: startTimeInMinutes,
+          endTime: endTimeInMinutes,
+        };
+      }
+    });
+    return sanitizedSchedule;
   };
 
   const initialDayValues = {
     active: false,
-    startTime: '00:00',
-    endTime: '00:00',
+    startTime: '06:00',
+    endTime: '21:00',
   };
 
-  const times = [
-    '00:00',
-    '01:00',
-    '02:00',
-    '03:00',
-    '04:00',
-    '05:00',
-    '06:00',
-    '07:00',
-    '08:00',
-    '09:00',
-    '10:00',
-    '11:00',
-    '12:00',
-    '13:00',
-    '14:00',
-    '15:00',
-    '16:00',
-    '17:00',
-    '18:00',
-    '19:00',
-    '20:00',
-    '21:00',
-    '22:00',
-    '23:00',
-    '24:00',
-  ];
-  const [selectedStartTime, setSelectedStartTime] = useState(times[0]);
-  const [selectedEndTime, setSelectedEndTime] = useState(times[0]);
+  const [selectedStartTime, setSelectedStartTime] = useState(times[6]);
+  const [selectedEndTime, setSelectedEndTime] = useState(times[21]);
+
+  const handleScheduleClick = (e: any, index: number) => {
+    setSchedule(schedules[index]);
+  };
+
   console.log(selectedStartTime);
   console.log(selectedEndTime);
-
+  console.log(schedule);
   function DayOfWeekInput({ day, values, setFieldValue }: DayOfWeekInputProps) {
-    console.log(values);
+    // console.log(values);
     const handleCheckBox = (e: any) => {
       setFieldValue(`${day}.active`, e.target.checked);
     };
@@ -132,7 +234,7 @@ export default function Availability({ header }: Props): JSX.Element {
             {...getDisabled(!values[day].active)}
             id="startTimeSelect"
             name="startTime"
-            value={selectedStartTime}
+            value={values[day].startTime}
             onChange={(e) => handleChange(e)}
           >
             {times.map((time) => {
@@ -158,7 +260,7 @@ export default function Availability({ header }: Props): JSX.Element {
             {...getDisabled(!values[day].active)}
             id="endTimeSelect"
             name="endTime"
-            value={selectedEndTime}
+            value={values[day].endTime}
             onChange={(e) => handleChange(e)}
           >
             {times.map((time) => {
@@ -177,22 +279,6 @@ export default function Availability({ header }: Props): JSX.Element {
             })}
           </Select>
         </FormControl>
-        {/* <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <DesktopTimePicker
-            className={classes.timePicker}
-            label="end Time"
-            value={values[day].startTime}
-            onChange={(e) => handleStartTime(e)}
-            renderInput={(params) => <TextField {...params} />}
-          />
-          <DesktopTimePicker
-            // className={classes.timePicker}
-            label="End Time"
-            value={values[day].endTime}
-            onChange={(e) => handleEndTime(e)}
-            renderInput={(params) => <TextField {...params} />}
-          />
-        </LocalizationProvider> */}
       </Box>
     );
   }
@@ -218,7 +304,23 @@ export default function Availability({ header }: Props): JSX.Element {
         >
           <FormControl className={classes.select}>
             <InputLabel id="scheduleName">Select</InputLabel>
-            <Select id="scheduleName" name="schedule"></Select>
+            <Select id="scheduleName" name="schedule">
+              {schedules &&
+                schedules.map((schedule, index) => {
+                  return (
+                    <MenuItem
+                      onClick={(e) => {
+                        handleScheduleClick(e, index);
+                      }}
+                      key={schedule._id}
+                      id={schedule._id}
+                      value={schedule._id}
+                    >
+                      {schedule.name}
+                    </MenuItem>
+                  );
+                })}
+            </Select>
           </FormControl>
           <Button
             onClick={() => handleNewSchedule()}
@@ -228,6 +330,53 @@ export default function Availability({ header }: Props): JSX.Element {
             Add Schedule +
           </Button>
         </Box>
+        {schedule && !newSchedule && (
+          <>
+            <Formik initialValues={schedule} onSubmit={handleEditSchedule}>
+              {({ values, setFieldValue, handleChange, handleSubmit, isSubmitting }) => (
+                <form className={classes.form} onSubmit={handleSubmit}>
+                  <FormInput
+                    className={classes.scheduleNameInput}
+                    id="scheduleSelect"
+                    label="Schedule"
+                    margin="dense"
+                    name="name"
+                    placeholder="Schedule name"
+                    autoComplete="name"
+                    autoFocus
+                    value={values.name}
+                    onChange={handleChange}
+                  />
+                  {showActive && <StarIcon></StarIcon>}
+                  {daysOfTheWeek.map((day) => {
+                    return (
+                      <DayOfWeekInput
+                        values={values}
+                        setFieldValue={setFieldValue}
+                        key={day}
+                        day={day}
+                      ></DayOfWeekInput>
+                    );
+                  })}
+                  <Box
+                    display="flex"
+                    flexDirection="row"
+                    justifyContent="center"
+                    // alignItems="center"
+                    height="100%"
+                  >
+                    <Button onClick={() => handleMakeActiveSchedule(values)}>
+                      Make Active <StarIcon></StarIcon>
+                    </Button>
+                    <Button>
+                      Delete Schedule <DeleteIcon></DeleteIcon>
+                    </Button>
+                  </Box>
+                </form>
+              )}
+            </Formik>
+          </>
+        )}
         {newSchedule && (
           <>
             <Formik
@@ -269,7 +418,7 @@ export default function Availability({ header }: Props): JSX.Element {
                   endTime: initialDayValues.endTime,
                 },
               }}
-              onSubmit={handleSubmit}
+              onSubmit={handleNewSubmit}
             >
               {({ values, setFieldValue, handleChange, handleSubmit, isSubmitting }) => (
                 <form className={classes.form} onSubmit={handleSubmit}>
@@ -295,23 +444,28 @@ export default function Availability({ header }: Props): JSX.Element {
                       ></DayOfWeekInput>
                     );
                   })}
+                  <Box
+                    display="flex"
+                    flexDirection="row"
+                    justifyContent="center"
+                    // alignItems="center"
+                    height="100%"
+                  >
+                    <Button type="submit" className={classes.saveButton}>
+                      Save Schedule
+                    </Button>
+                    {newScheduleCreated && (
+                      <Button onClick={() => handleMakeActiveSchedule(newScheduleId)}>
+                        Make Active Schedule<StarIcon></StarIcon>
+                      </Button>
+                    )}
+                    <Button>
+                      Delete Schedule <DeleteIcon></DeleteIcon>
+                    </Button>
+                  </Box>
                 </form>
               )}
             </Formik>
-            <Box
-              display="flex"
-              flexDirection="row"
-              justifyContent="center"
-              // alignItems="center"
-              height="100%"
-            >
-              <Button className={classes.saveButton}>
-                Save Schedule <StarIcon></StarIcon>
-              </Button>
-              <Button>
-                Delete Schedule <DeleteIcon></DeleteIcon>
-              </Button>
-            </Box>
           </>
         )}
       </Box>
