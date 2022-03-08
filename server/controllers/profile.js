74489b9a45719a783065d5f3819eca14a1f88e12
Nathan Brickett <natey37@gmail.com>
2022-03-07 18:57:18 -0800

    add the end date to the filter query

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -28,7 +28,7 @@ exports.editProfile = asyncHandler(async (req, res, next) => {
 // @access Private
 exports.loadProfile = asyncHandler(async (req, res, next) => {
   const profile = await Profile.findOne({ userId: req.user.id });
-  
+
   if (!profile) {
     res.status(401);
     throw new Error("Profile not found!");
@@ -47,24 +47,40 @@ const daysOfTheWeek = {
   6: 'saturday',
   7: 'sunday',
 }
-// @route GET /profiles/sitters?location=address&dates=date-range
+// @route GET /profiles/sitters?location=address&startDate=date&endDate=date
 // @desc Get user profiles
 // @access Public
 exports.loadProfiles = asyncHandler(async (req, res, next) => {
   let petSitters = await Profile.find({ accountType: 'pet_sitter', address: { $exists: true }, price: { $exists: true } })
 
-  if (req.query.location !== 'undefined') {
+  if (req.query.location !== 'null' && req.query.location !== 'undefined' && req.query.location !== '') {
     petSitters = petSitters.filter((sitter) => sitter.address.includes(req.query.location))
   }
-  if (req.query.dates !== 'undefined') {
-    const date = new Date(req.query.dates)
-    const dayOfTheWeek = daysOfTheWeek[date.getDay()]
+  if (req.query.startDate !== 'null' && req.query.startDate !== 'undefined' && req.query.startDate !== '') {
+    const startDate = new Date(req.query.startDate)
+    const endDate = new Date(req.query.endDate)
+    const startDateNumber = startDate.getDay()
+    const endDateNumber = endDate.getDay()
+
+    const startDayOfTheWeek = daysOfTheWeek[startDateNumber]
+    const endDayOfTheWeek = daysOfTheWeek[endDateNumber]
 
     const availabilities = await Availability.find()
     const availableProfiles = []
     availabilities.forEach((schedule) => {
-      if (schedule[dayOfTheWeek].active) {
-        availableProfiles.push(JSON.stringify(schedule.profileId))
+      if (startDayOfTheWeek === endDayOfTheWeek) {
+        if (schedule[startDayOfTheWeek].active) {
+          availableProfiles.push(JSON.stringify(schedule.profileId))
+        }
+      } else {
+        let available = true
+        for (let i = startDateNumber; i <= endDateNumber; i++) {
+          if (!schedule[daysOfTheWeek[i]].active) {
+            available = false
+            break;
+          }
+        }
+        if (available) availableProfiles.push(JSON.stringify(schedule.profileId))
       }
     })
 
